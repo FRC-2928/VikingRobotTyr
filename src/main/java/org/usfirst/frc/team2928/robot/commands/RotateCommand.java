0a16a5b44f3235ea209bdddf9f0c832078759151
Justin Sonntag <justin.sonntag@gmail.com>
2017-01-13 00:59:59 -0800

    Fix up rotational gyro code.

--- src/main/java/org/usfirst/frc/team2928/robot/commands/RotateCommand.java
+++ src/main/java/org/usfirst/frc/team2928/robot/commands/RotateCommand.java
@@ -3,62 +3,37 @@ package org.usfirst.frc.team2928.robot.commands;
 import org.usfirst.frc.team2928.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RotateCommand extends Command {
 
 	public RotateCommand() {
-		// TODO Auto-generated constructor stub
-		//requires(Robot.drivebase);
 		requires(Robot.gyroPid);
 		Robot.gyroPid.enable();
-		SmartDashboard.putNumber("Gyro Constructor", 7);
-		
 	}
-
-	
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-		//Temporary Reset
-		Robot.gyroPid.resetGyro(90);
-		SmartDashboard.putNumber("Gyro Initalize", 12);
+		Robot.gyroPid.calibrate();
+		Robot.gyroPid.setSetpoint(90);
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generted method stub
-		SmartDashboard.putNumber("Gyro Execute", 10);
-		SmartDashboard.putNumber("Gyro Hash", this.hashCode());
-		double pidInput = Robot.gyroPid.returnPIDInput();
-		SmartDashboard.putNumber("Gyro PID Input", pidInput);
-		Robot.gyroPid.usePIDOutput(pidInput);
-	}
+
+    }
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stubo
-		double diff = Robot.gyroPid.returnPIDInput() ;
-		SmartDashboard.putNumber("Finished", 12);
-		double target = Robot.gyroPid.getTargetAngle();
-		if(diff < 10 && diff > -10){
-			
-			return true;
-		}
-		
-		return false;
+	    return Robot.gyroPid.onTarget();
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
-
-	}
+	    Robot.drivebase.stop();
+    }
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 		end();
 	}
 

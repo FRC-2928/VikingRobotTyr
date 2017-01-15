e25a4c9d3e9ad38c8fbeaab04dd04e7803ce4228
Max A <infinitelyExplosive@gmail.com>
2017-01-14 17:35:23 -0800

    Removed auto-generated TODOS from LifterCommand

--- src/main/java/org/usfirst/frc/team2928/robot/commands/LifterCommand.java
+++ src/main/java/org/usfirst/frc/team2928/robot/commands/LifterCommand.java
@@ -6,51 +6,42 @@ public class LifterCommand extends Command {
 
 	private final static int lifterHighTarget = 15274;
 	public LifterCommand() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public LifterCommand(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	public LifterCommand(double timeout) {
 		super(timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	public LifterCommand(String name, double timeout) {
 		super(name, timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 

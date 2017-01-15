d210ac05fec6a0f5d072cbb152bb790248d008f1
Max A <infinitelyExplosive@gmail.com>
2017-01-14 17:39:12 -0800

    Removed auto-generated TODOs from ResetShifterCommand

--- src/main/java/org/usfirst/frc/team2928/robot/commands/ResetShifterCommand.java
+++ src/main/java/org/usfirst/frc/team2928/robot/commands/ResetShifterCommand.java
@@ -7,54 +7,45 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ResetShifterCommand extends Command {
 
 	public ResetShifterCommand() {
-		// TODO Auto-generated constructor stub
 		requires(Robot.shift);
 	}
 
 	public ResetShifterCommand(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ResetShifterCommand(double timeout) {
 		super(timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ResetShifterCommand(String name, double timeout) {
 		super(name, timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
 		Robot.shift.reset();
 
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		
+
 
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 

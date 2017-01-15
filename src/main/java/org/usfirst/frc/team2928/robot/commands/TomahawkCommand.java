782d4d0cdb151918a7a603052adac8c2d63ae410
Max A <infinitelyExplosive@gmail.com>
2017-01-14 17:41:10 -0800

    Removed auto-generated TODOs from TomahawkCommand

--- src/main/java/org/usfirst/frc/team2928/robot/commands/TomahawkCommand.java
+++ src/main/java/org/usfirst/frc/team2928/robot/commands/TomahawkCommand.java
@@ -15,22 +15,18 @@ public class TomahawkCommand extends Command {
 		super(name);
 		requires(Robot.shooterMotors);
 
-		// TODO Auto-generated constructor stub
 	}
 
 	public TomahawkCommand(double timeout) {
 		super(timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	public TomahawkCommand(String name, double timeout) {
 		super(name, timeout);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
 		if (raise)
 		{
 			Robot.tomahawk.Raise();
@@ -42,24 +38,20 @@ public class TomahawkCommand extends Command {
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 	}
 
 }
